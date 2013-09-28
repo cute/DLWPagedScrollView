05817df0c9ce4073301e65280ba44c866be47f5b
Guangming Li <leeful@gmail.com>
2013-09-28 13:59:34 +0800

    Initial Commit

---
+++ DLWPagedScrollView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DLWPagedScrollView
+//
+//  Created by dengliwa on 13-9-28.
+//  Copyright (c) 2013年 dengliwa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DLWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DLWAppDelegate class]));
+    }
+}
